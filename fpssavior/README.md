cd0731a154360c606dbd04c2b5d00efb57a40891
xinxs <gustavoxin@hotmail.com>
2020-07-31 09:00:41 -0300

    fpssavior update

--- fpssavior/README.md
+++ fpssavior/README.md
@@ -2,16 +2,27 @@
 
 Updated version of fpssavior from [FiftyCaliber](https://github.com/FiftyCaliber).
 
-Removed a broken graphic option, added toggle button and new mode: "Medium".
+Removed a broken graphic option, added new options.
 
-**In "Ultra Low" mode, Players drawn is set to -100 which make the game try to not shown anyone besides you, this mode also remove all effects (skills included). Only recommended for world boss hunt**
+New func: Hide players/summons
+
+![](https://i.imgur.com/JJN234g.jpg)
+
+**Notes to use hide func.:
+
+**To unhide things, you need to get away from them and come back according to your field view. In some instances like the CM, which has no limit on the rendering distance, you will need to relog, since its will not update just by geting away. Summons will need to re summon, since you cant get far from them.
+
+**Also note that some bosses mechanics will also be hide by using the summon option, like White Witch's orbs.
+
+![](https://i.imgur.com/JJN234g.jpg)
 
 /fpssavior lock : lock/unlock the ui to move around.   /fpssavior : toggle betwen modes 
 
-![](https://media.giphy.com/media/VJfF1fo37q2DjdZkuW/giphy.webp)
 
 -v2.1.0: switched the toggle button to 4 select buttons: 
 
 ![](https://i.imgur.com/2X5lJrR.jpg)
 
 -v2.2.0: low/ultralow sets graphics > low mode to on (lighter textures);
+
+-v2.3.1: Added hide func. Added some new graphics options inside presets. Adjusted presets players draw, since hide func will make the job.
--- fpssavior/src/fpssavior.lua
+++ fpssavior/src/fpssavior.lua
@@ -1,5 +1,12 @@
-_G["FPSSAVIOR"] = {};
-_G["FPSSAVIOR"]["saviorMode"] = {"{#2D9B27}{ol}HIGH","{#F2F407}{ol}MEDIUM","{#284B7E}{ol}LOW","{#532881}{ol}UltraLOW"};
+local addonName = "FPSSAVIOR"
+local author = "XINXS"
+
+_G["ADDONS"] = _G["ADDONS"] or {}
+_G["ADDONS"][author] = _G["ADDONS"][author] or {}
+_G["ADDONS"][author][addonName] = _G["ADDONS"][author][addonName] or {}
+local g = _G["ADDONS"][author][addonName];
+g.saviormode = {"{#2D9B27}{ol}HIGH","{#F2F407}{ol}MEDIUM","{#284B7E}{ol}LOW","{#532881}{ol}UltraLOW"};
+
 local acutil = require("acutil");
 CHAT_SYSTEM("FPS Savior loaded! Help: /fpssavior help");
 
@@ -7,9 +14,8 @@ function FPSSAVIOR_ON_INIT(addon, frame)
 	frame:ShowWindow(1);
 	acutil.slashCommand("/fpssavior",FPSSAVIOR_CMD);
 	addon:RegisterMsg("GAME_START", "FPSSAVIOR_START");
-	addon:RegisterMsg("FPS_UPDATE", "FPSSAVIOR_SHOWORHIDE_FRAMES");
+	addon:RegisterMsg("FPS_UPDATE", "FPSSAVIOR_SHOWORHIDE_OBJ");
 	
-	_G["FPSSAVIOR"]["hidden"] = {};
 	FPSSAVIOR_LOADSETTINGS();
 end
 
@@ -17,21 +23,31 @@ function FPSSAVIOR_LOADSETTINGS()
 	local settings, error = acutil.loadJSON("../addons/fpssavior/settings.json");
 	if error then
 		FPSSAVIOR_SAVESETTINGS();
-	else
-		_G["FPSSAVIOR"]["settings"] = settings;
+		return
 	end
+	if settings.allplayers == nil then
+		settings.allplayers = 0;
+		settings.onlypt = 0;
+		settings.allsum = 0;
+		settings.onlymy = 0;
+	end
+		g.settings = settings;
 end
 
 function FPSSAVIOR_SAVESETTINGS()
-	if _G["FPSSAVIOR"]["settings"] == nil then
-		_G["FPSSAVIOR"]["settings"] = {
-			saviorToggle = 2;
-			displayX = 510;
-			displayY = 920;
-			lock = 1;
+	if g.settings == nil then
+		g.settings = {
+			saviorToggle = 2,
+			displayX = 510,
+			displayY = 920,
+			lock = 1,
+			allplayers = 0,
+			onlypt = 0,
+			allsum = 0,
+			onlymy = 0
 		};
 	end
-	acutil.saveJSON("../addons/fpssavior/settings.json", _G["FPSSAVIOR"]["settings"]);
+	acutil.saveJSON("../addons/fpssavior/settings.json", g.settings);
 end
 
 function FPSSAVIOR_START_DRAG()
@@ -39,8 +55,8 @@ function FPSSAVIOR_START_DRAG()
 end
 
 function FPSSAVIOR_END_DRAG()
-	_G["FPSSAVIOR"]["settings"].displayX = saviorFrame:GetX();
-	_G["FPSSAVIOR"]["settings"].displayY = saviorFrame:GetY();
+	g.settings.displayX = saviorFrame:GetX();
+	g.settings.displayY = saviorFrame:GetY();
 	FPSSAVIOR_SAVESETTINGS();
 	saviorFrame.isDragging = false;
 end
@@ -51,20 +67,25 @@ function FPSSAVIOR_START()
 		saviorFrame = ui.CreateNewFrame("fpssavior","fpssaviorframe");
 		saviorFrame:SetBorder(0, 0, 0, 0);
 		saviorFrame:Resize(100,60)
-		saviorFrame:SetOffset(_G["FPSSAVIOR"]["settings"].displayX, _G["FPSSAVIOR"]["settings"].displayY);
+		saviorFrame:SetOffset(g.settings.displayX, g.settings.displayY);
 		saviorFrame:ShowWindow(1);
 		saviorFrame:SetLayerLevel(61);
 		saviorFrame.isDragging = false;
 		saviorFrame:SetEventScript(ui.LBUTTONDOWN, "FPSSAVIOR_START_DRAG");
 		saviorFrame:SetEventScript(ui.LBUTTONUP, "FPSSAVIOR_END_DRAG");
 		saviorFrame:EnableMove(0);
-		_G["FPSSAVIOR"]["settings"].lock = 1;
+		g.settings.lock = 1;
 		FPSSAVIOR_SAVESETTINGS();
 		
 		saviorText = saviorFrame:CreateOrGetControl("richtext","saviortext",0,0,0,0);
 		saviorText = tolua.cast(saviorText,"ui::CRichText");
-		saviorText:SetGravity(ui.CENTER_HORZ,ui.CENTER_VERT);
-		saviorText:SetText("{s18}".._G["FPSSAVIOR"]["saviorMode"][_G["FPSSAVIOR"]["settings"].saviorToggle]);
+		saviorText:SetGravity(ui.LEFT,ui.CENTER_VERT);
+		saviorText:SetText("{s18}".. g.saviormode[g.settings.saviorToggle]);
+		
+		btnsaviorC = saviorFrame:CreateOrGetControl('button', 'btnc', 0, 0, 16, 16); 
+		btnsaviorC:SetOffset(84, 20);
+		btnsaviorC:SetText(string.format("{img chat_option2_btn %d %d}", 16, 16));
+		btnsaviorC:SetEventScript(ui.LBUTTONDOWN, 'FPSSAVIOR_OPENCONFIG');
 		
 		btnsaviorH = saviorFrame:CreateOrGetControl('button', 'btnh', 0, 0, 25, 20); 
 		btnsaviorH:SetOffset(0, 40); 
@@ -87,17 +108,131 @@ function FPSSAVIOR_START()
 		btnsaviorUlow:SetEventScript(ui.LBUTTONDOWN, 'FPSSAVIOR_ULOW');
 	end
 	if not saviorFrame.isDragging then
-		saviorFrame:SetOffset(_G["FPSSAVIOR"]["settings"].displayX, _G["FPSSAVIOR"]["settings"].displayY);
+		saviorFrame:SetOffset(g.settings.displayX, g.settings.displayY);
 	end
-	if _G["FPSSAVIOR"]["settings"].saviorToggle == 1 then
+	if g.settings.saviorToggle == 1 then
 		FPSSAVIOR_HIGH();
-	elseif _G["FPSSAVIOR"]["settings"].saviorToggle == 2 then
+	elseif g.settings.saviorToggle == 2 then
 		FPSSAVIOR_MEDIUM();
-	elseif _G["FPSSAVIOR"]["settings"].saviorToggle == 3 then
+	elseif g.settings.saviorToggle == 3 then
 		FPSSAVIOR_LOW();
 	else
 		FPSSAVIOR_ULOW();
 	end
+end
+
+function FPSSAVIOR_OPENCONFIG()
+	local configframe = ui.GetFrame('fpssaviorconfig');
+	if  configframe ~= nil and configframe:IsVisible() == 1  then
+		configframe:ShowWindow(0);
+	else
+		FPSSAVIOR_CONFIGFRAME();
+	end
+end
+
+function FPSSAVIOR_CONFIGFRAME()
+	local configframe = ui.GetFrame('fpssaviorconfig');
+	if configframe == nil then
+		configframe = ui.CreateNewFrame("fpssavior","fpssaviorconfig");
+		configframe:SetSkinName("pip_simple_frame");
+		configframe:Resize(170,152);
+		configframe:SetLayerLevel(62);
+	end
+	configframe:ShowWindow(1);
+	local mainX = ui.GetFrame("fpssaviorframe"):GetX();
+	local mainY = ui.GetFrame("fpssaviorframe"):GetY();
+	if mainY < 210 then
+		configframe:SetOffset(mainX,mainY+60);
+	else
+		configframe:SetOffset(mainX,mainY-134);
+	end
+	
+	local playerText = configframe:CreateOrGetControl("richtext","playertext",5,10,0,0);
+	playerText:SetText("{ol}Players:");
+	local playerBox = configframe:CreateOrGetControl('checkbox', 'allplayersbox', 5, 32, 100, 20)
+	playerBox:SetText("{ol}Hide all");
+	playerBox:SetEventScript(ui.LBUTTONUP,"FPSSAVIOR_SAVECONFIGFRAME1");
+	--playerBox:SetEventScriptArgString(ui.LBUTTONUP, "allplayers");
+	local playerBoxchild = GET_CHILD(configframe, "allplayersbox");
+	playerBoxchild:SetCheck(g.settings.allplayers);
+	playerBox = configframe:CreateOrGetControl('checkbox', 'onlyptbox', 5, 54, 100, 20)
+	playerBox:SetText("{ol}Show pt/guild only");
+	playerBox:SetEventScript(ui.LBUTTONUP,"FPSSAVIOR_SAVECONFIGFRAME2");
+	--playerBox:SetEventScriptArgString(ui.LBUTTONUP, "onlypt");
+	playerBoxchild = GET_CHILD(configframe, "onlyptbox");
+	playerBoxchild:SetCheck(g.settings.onlypt);
+		
+	local summonText = configframe:CreateOrGetControl("richtext","summontext",5,80,0,0);
+	summonText:SetText("{ol}Summons:");
+	local summonBox = configframe:CreateOrGetControl('checkbox', 'allsumbox', 5, 102, 100, 20)
+	summonBox:SetText("{ol}Hide all");
+	summonBox:SetEventScript(ui.LBUTTONUP,"FPSSAVIOR_SAVECONFIGFRAME3");
+	--summonBox:SetEventScriptArgString(ui.LBUTTONUP, "allsum");
+	local summonBoxchild = GET_CHILD(configframe, "allsumbox");
+	summonBoxchild:SetCheck(g.settings.allsum);
+	summonBox = configframe:CreateOrGetControl('checkbox', 'onlymybox', 5, 124, 100, 20)
+	summonBox:SetText("{ol}Show mine only");
+	summonBox:SetEventScript(ui.LBUTTONUP,"FPSSAVIOR_SAVECONFIGFRAME4");
+	--summonBox:SetEventScriptArgString(ui.LBUTTONUP, "onlymy");
+	summonBoxchild = GET_CHILD(configframe, "onlymybox");
+	summonBoxchild:SetCheck(g.settings.onlymy);	
+end
+
+function FPSSAVIOR_SAVECONFIGFRAME1()
+	local configframe = ui.GetFrame('fpssaviorconfig');
+	local playerBoxchild = GET_CHILD(configframe, "allplayersbox");
+	if playerBoxchild:IsChecked() == 1 then
+			g.settings.allplayers = 1;
+			g.settings.onlypt = 0;
+			playerBoxchild = GET_CHILD(configframe, "onlyptbox");
+			playerBoxchild:SetCheck(g.settings.onlypt);
+	else
+			g.settings.allplayers = 0;
+	end
+	acutil.saveJSON("../addons/fpssavior/settings.json", g.settings);
+	--FPSSAVIOR_CONFIGFRAME()
+end
+function FPSSAVIOR_SAVECONFIGFRAME2()
+	local configframe = ui.GetFrame('fpssaviorconfig');
+	local playerBoxchild = GET_CHILD(configframe, "onlyptbox");
+	if playerBoxchild:IsChecked() == 1 then
+			g.settings.allplayers = 0;
+			g.settings.onlypt = 1;
+			playerBoxchild = GET_CHILD(configframe, "allplayersbox");
+			playerBoxchild:SetCheck(g.settings.allplayers);
+	else
+			g.settings.onlypt = 0;
+	end
+	acutil.saveJSON("../addons/fpssavior/settings.json", g.settings);
+	--FPSSAVIOR_CONFIGFRAME()
+end
+function FPSSAVIOR_SAVECONFIGFRAME3()
+	local configframe = ui.GetFrame('fpssaviorconfig');
+	local summonBoxchild = GET_CHILD(configframe, "allsumbox");
+	if summonBoxchild:IsChecked() == 1 then
+			g.settings.allsum = 1;
+			g.settings.onlymy = 0;
+			summonBoxchild = GET_CHILD(configframe, "onlymybox");
+			summonBoxchild:SetCheck(g.settings.onlymy);
+	else
+			g.settings.allsum = 0;
+	end
+	acutil.saveJSON("../addons/fpssavior/settings.json", g.settings);
+	--FPSSAVIOR_CONFIGFRAME()
+end
+function FPSSAVIOR_SAVECONFIGFRAME4()
+	local configframe = ui.GetFrame('fpssaviorconfig');
+	local summonBoxchild = GET_CHILD(configframe, "onlymybox");
+	if summonBoxchild:IsChecked() == 1 then
+			g.settings.allsum = 0;
+			g.settings.onlymy = 1;
+			summonBoxchild = GET_CHILD(configframe, "allsumbox");
+			summonBoxchild:SetCheck(g.settings.allsum);
+	else
+			g.settings.onlymy = 0;
+	end
+	acutil.saveJSON("../addons/fpssavior/settings.json", g.settings);
+	--FPSSAVIOR_CONFIGFRAME()
 end
 
 function FPSSAVIOR_CMD(command)
@@ -109,12 +244,12 @@ function FPSSAVIOR_CMD(command)
 		return;
 	end
 	if cmd == "help" then
-		CHAT_SYSTEM("FPS Savior Help:{nl}'/fpssavior' to toggle between 3 predefined settings High, Low, and Super Low.{nl}'/fpssavior lock' to unlock/lock the settings display in order to move it around.{nl}'/fpssavior default' to restore the settings display to its default location.");
+		CHAT_SYSTEM("FPS Savior Help:{nl}'/fpssavior' to toggle between 3 predefined settings High, Low, and Ultra Low.{nl}'/fpssavior lock' to unlock/lock the settings display in order to move it around.{nl}'/fpssavior default' to restore the settings display to its default location.");
 		return;
 	end
 	if cmd == "lock" then
-		if _G["FPSSAVIOR"]["settings"].lock == 1 then
-			_G["FPSSAVIOR"]["settings"].lock = 0;
+		if g.settings.lock == 1 then
+			g.settings.lock = 0;
 			saviorFrame:EnableHitTest(1);
 			saviorText:EnableHitTest(0);
 			saviorFrame:EnableMove(1);
@@ -122,7 +257,7 @@ function FPSSAVIOR_CMD(command)
 			CHAT_SYSTEM("Settings display unlocked.");
 			FPSSAVIOR_SAVESETTINGS();
 		else
-			_G["FPSSAVIOR"]["settings"].lock = 1;
+			g.settings.lock = 1;
 			saviorFrame:EnableHitTest(1);
 			saviorFrame:EnableMove(0);
 			saviorFrame.EnableHittestFrame(saviorFrame, 1);
@@ -132,10 +267,10 @@ function FPSSAVIOR_CMD(command)
 		return;
 	end
 	if cmd == "default" then
-		_G["FPSSAVIOR"]["settings"].displayX = 510;
-		_G["FPSSAVIOR"]["settings"].displayY = 920;
-		_G["FPSSAVIOR"]["settings"].lock = 1;
-		saviorFrame:SetOffset(_G["FPSSAVIOR"]["settings"].displayX, _G["FPSSAVIOR"]["settings"].displayY);
+		g.settings.displayX = 510;
+		g.settings.displayY = 920;
+		g.settings.lock = 1;
+		saviorFrame:SetOffset(g.settings.displayX, g.settings.displayY);
 		saviorFrame:EnableHitTest(1);
 		saviorFrame:EnableMove(0);
 		saviorFrame.EnableHittestFrame(saviorFrame, 1);
@@ -148,13 +283,13 @@ end
 
 function FPSSAVIOR_SETTEXT()
 	if saviorFrame ~= nil then
-		saviorText:SetText("{s18}".._G["FPSSAVIOR"]["saviorMode"][_G["FPSSAVIOR"]["settings"].saviorToggle]);
+		saviorText:SetText("{s18}".. g.saviormode[g.settings.saviorToggle]);
 	end
 end
 
 
 function FPSSAVIOR_HIGH()
-	_G["FPSSAVIOR"]["settings"].saviorToggle = 1;
+	g.settings.saviorToggle = 1;
 
 	graphic.SetDrawActor(100);
 	graphic.SetDrawMonster(100);
@@ -171,6 +306,9 @@ function FPSSAVIOR_HIGH()
 	graphic.EnableWater(1);  
 	imcperfOnOff.EnableIMCEffect(1);
 	imcperfOnOff.EnableEffect(1);
+	imcperfOnOff.EnableRenderShadow(1); 
+	imcperfOnOff.EnablePlaneLight(1);
+	imcperfOnOff.EnableWater(1);
 	
 	--lowmodeconfig
 	graphic.EnableLowOption(0);
@@ -182,9 +320,9 @@ function FPSSAVIOR_HIGH()
 end
 
 function FPSSAVIOR_MEDIUM()
-	_G["FPSSAVIOR"]["settings"].saviorToggle = 2;
+	g.settings.saviorToggle = 2;
 		
-	graphic.SetDrawActor(30);
+	graphic.SetDrawActor(40);
 	graphic.SetDrawMonster(50);
 	graphic.EnableFastLoading(1);
 	graphic.EnableBlur(0);
@@ -199,6 +337,9 @@ function FPSSAVIOR_MEDIUM()
 	graphic.EnableWater(0); 
 	imcperfOnOff.EnableIMCEffect(1);
 	imcperfOnOff.EnableEffect(1);
+	imcperfOnOff.EnableRenderShadow(0);
+	imcperfOnOff.EnablePlaneLight(1);
+	imcperfOnOff.EnableWater(1);
 		
 	--lowmodeconfig
 	graphic.EnableLowOption(0);
@@ -210,9 +351,9 @@ function FPSSAVIOR_MEDIUM()
 end
 
 function FPSSAVIOR_LOW()
-	_G["FPSSAVIOR"]["settings"].saviorToggle = 3;
+	g.settings.saviorToggle = 3;
 		
-	graphic.SetDrawActor(15);
+	graphic.SetDrawActor(30);
 	graphic.SetDrawMonster(30);
 	graphic.EnableFastLoading(1);
 	graphic.EnableBlur(0);
@@ -227,6 +368,9 @@ function FPSSAVIOR_LOW()
 	graphic.EnableWater(0); 
 	imcperfOnOff.EnableIMCEffect(1);
 	imcperfOnOff.EnableEffect(1);
+	imcperfOnOff.EnableRenderShadow(0); 
+	imcperfOnOff.EnablePlaneLight(0);
+	imcperfOnOff.EnableWater(0);
 		
 	--lowmodeconfig
 	graphic.EnableLowOption(1);
@@ -238,9 +382,9 @@ function FPSSAVIOR_LOW()
 end
 
 function FPSSAVIOR_ULOW()
-	_G["FPSSAVIOR"]["settings"].saviorToggle = 4;
+	g.settings.saviorToggle = 4;
 		
-	graphic.SetDrawActor(-100);
+	graphic.SetDrawActor(20);
 	graphic.SetDrawMonster(30);
 	graphic.EnableFastLoading(1);
 	graphic.EnableBlur(0);
@@ -255,6 +399,9 @@ function FPSSAVIOR_ULOW()
 	graphic.EnableWater(0); 
 	imcperfOnOff.EnableIMCEffect(0);
 	imcperfOnOff.EnableEffect(0);
+	imcperfOnOff.EnableRenderShadow(0); 
+	imcperfOnOff.EnablePlaneLight(0);
+	imcperfOnOff.EnableWater(0);
 		
 	--lowmodeconfig
 	graphic.EnableLowOption(1);
@@ -266,60 +413,36 @@ function FPSSAVIOR_ULOW()
 end
 
 function FPSSAVIOR_TOGGLE()
-	if _G["FPSSAVIOR"]["settings"].saviorToggle == 1 then
+	if g.settings.saviorToggle == 1 then
 		FPSSAVIOR_MEDIUM();
-	elseif _G["FPSSAVIOR"]["settings"].saviorToggle == 2 then
+	elseif g.settings.saviorToggle == 2 then
 		FPSSAVIOR_LOW();
-	elseif _G["FPSSAVIOR"]["settings"].saviorToggle == 3 then
+	elseif g.settings.saviorToggle == 3 then
 		FPSSAVIOR_ULOW();
 	else
 		FPSSAVIOR_HIGH();
 	end
 end
 
-function FPSSAVIOR_SHOWORHIDE_FRAMES()
-	if _G["FPSSAVIOR"]["settings"].saviorToggle == 4 then
-		_G["FPSSAVIOR"].wasHidden = true;
-		for i = 0,200 do
-			local charbaseinfo = ui.GetFrame("charbaseinfo1_"..i);
-			if charbaseinfo ~= nil then
-				if charbaseinfo:IsVisible() == 1 then
-					table.insert(_G["FPSSAVIOR"]["hidden"],"charbaseinfo1_"..i);
-					charbaseinfo:ShowWindow(0);
+function FPSSAVIOR_SHOWORHIDE_OBJ()
+	if (g.settings.allplayers + g.settings.onlypt + g.settings.allsum + g.settings.onlymy) > 0 then
+		local list, cnt = SelectObject(GetMyPCObject(), 650, "ALL");
+		for i = 1, cnt do			
+			local ObHandle = GetHandle(list[i]);
+			local OwHandle = info.GetOwner(ObHandle);
+			if (g.settings.allplayers + g.settings.onlypt) > 0 and info.IsPC(ObHandle) == 1 then
+				if g.settings.allplayers == 1 then
+					world.Leave(ObHandle, 0.0 );
+				elseif session.party.GetPartyMemberInfoByName(PARTY_NORMAL, info.GetFamilyName(ObHandle)) == nil and session.party.GetPartyMemberInfoByName(PARTY_GUILD, info.GetFamilyName(ObHandle)) == nil then
+					world.Leave(ObHandle, 0.0 );
+				end
+			elseif (g.settings.allsum + g.settings.onlymy) > 0 and OwHandle ~= 0 then
+				if g.settings.allsum == 1 then
+					world.Leave(ObHandle, 0.0 );
+				elseif OwHandle ~= session.GetMyHandle() then
+					world.Leave(ObHandle, 0.0 );
 				end
 			end
-		end
-		local selectedObjects, selectedObjectsCount = SelectObject(GetMyPCObject(), 1000000, "ALL");
-		for i = 1, selectedObjectsCount do
-			local handle = GetHandle(selectedObjects[i]);
-			if handle ~= nil then
-				if info.IsPC(handle) == 1 then
-					local shopFrame = ui.GetFrame("SELL_BALLOON_"..handle);
-					if shopFrame ~= nil then
-						if shopFrame:IsVisible() == 1 then
-							table.insert(_G["FPSSAVIOR"]["hidden"],"SELL_BALLOON_"..handle);
-							shopFrame:ShowWindow(0);
-						end
-					end
-					local ytxtFrame = ui.GetFrame(handle.."_pctitle");
-					if ytxtFrame ~= nil then
-						if ytxtFrame:IsVisible() == 1 then
-							table.insert(_G["FPSSAVIOR"]["hidden"],handle.."_pctitle");
-							ytxtFrame:ShowWindow(0);
-						end
-					end
-				end
-			end
-		end
-	else
-		if _G["FPSSAVIOR"].wasHidden == true then
-			for k,v in pairs(_G["FPSSAVIOR"]["hidden"]) do
-				local frame = ui.GetFrame(v);
-				if frame ~= nil then
-					frame:ShowWindow(1);
-				end
-			end
-			_G["FPSSAVIOR"].wasHidden = false;
-		end
-	end
+        end
+	end	
 end
